4cef5dd4a95c8b70bcbe23aadbe9d0473fba31a9
SteveDev <stevehoneck@gmail.com>
2021-04-05 23:53:15 -0400

    Post 'Exercise: Callbacks and Error Handling in Node'

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
-//Removed this when converting to a Node module format, used imports from "rectangle.js" instead.
-/*
-const rect = { //an object named rec with 2 methods
-    perimeter: (x, y) => 2 * (x + y), //The first method is "perimeter"
-    area: (x, y) => x * y // The second method is "area", made into an arrow function
-};
-*/
-
 const rect = require('./rectangle'); //"require" function from Node. "require" function will look for a file named "rectangle.js" in the same folder that this file is in and it will take the exports from that file and set them as properties, or methods, of this "rect" variable object.
 
-function solveRect(l, w) { //A function we created to make use of the "rect" object.  Pass in the values "l" and "w"
+//Order of operations for code:  Code will call the function in the rectangle module which will chek if the given length or width is 0 or less, then if so it will run the callback with an error object right away and a console.log with the error message will appear. If the numbers are valid, then the module will run the callback after a delay of 2 seconds, but it won't get blocked there for 2 seconds, the Node event loop will continue and print out the "This statement is logged after...." console.log. Then once the 2 second timer is up, Node will go to the timer phase of the event loop and will execute the callback that was passed into the timer fuction. The callback will solve for and console log the area and perimeter.
+
+function solveRect(l, w) { //A function we created to make use of the "rect" function.  Pass in the values "l" and "w"
     console.log(`Solving for rectangle with dimensions: ${l}, ${w}`); //log the values of "l" and "w" to the console using template literals (backticks, not quote marks)
 
-    if (l <= 0 || w <= 0) { //error checking, values of 0 or less are not valid. If either conditions are true, console log the error message
-        console.log(`Rectangle dimensions must be greater than zero. Received: ${l}, ${w}`);
-    } else { //Console log the area and perimeter of the angle using the methods defined on the "rect" object (via "rect.area", "rect.perimeter") passing in the "l" and "w" arguments
-        console.log(`Area of rectangle: ${rect.area(l, w)}`);
-        console.log(`Perimeter of rectangle: ${rect.perimeter(l, w)}`);
-    }
+    //"rect" is now the function in the "rectangle.js" file. It requires 3 arguments: two positive numbers and a callback function. The two numbers will be taken from what is passed in to the "solveRect" function calls (and reassigned as "x" and "y" in "rectangle.js"). The callback function is written in-line in the parameter list using an arrow function (it's not executed in the parameter list, it's only being defined here) It's executed within the "rectangle.js" file.
+    rect(l, w, (err, rectangle) => {
+        if (err) { //check if the callback funciton is being called with an error object
+            console.log('ERROR:', err.message); // if so, log the error message
+        } else { //Console log the area and perimeter of the rectangle using the methods defined on the "rect" object (via "rect.area", "rect.perimeter") passing in the "l" and "w" arguments
+            console.log(`Area of rectangle with dimensions ${l}, ${w} is: ${rectangle.area()}`); //Object that owns the ".area" and ".perimeter" methods is being passed into this callback function with the name "rectangle" as definedin the paramter list. Method calls have empty paramter lists because the deimensions are now defined in rectangle.js.
+            console.log(`Perimeter of rectangle with dimensions ${l}, ${w} is: ${rectangle.perimeter()}`);
+        }
+    });
+    console.log('This statement is logged after the call to rect()');
 }
 
 //Call the function multiple times for testing
--- rectangle.js
+++ rectangle.js
@@ -1,4 +1,16 @@
-//This file is considered a module with 2 exports.
+//Updated to use callbacks and handle errors
 
-exports.perimeter = (x, y) => 2 * (x + y); //"exports" is a keyword from Node, it is a reference for "module.exports", but just "exports" can be used as a short hand. "perimeter" is the method name being exported.
-exports.area = (x, y) => x * y;//"area" is the method name being exported.
+module.exports = (x, y, callback) => { //Assigning a function as the entire value of the export (instead of two separate methods of the export). Therefor, Cant use shorthand "exports" must use "module.exports". Argument "callback" is a function that will be passed in when this module is called (it will be defined elsewhere, not in-line in the argument list).
+    if (x <= 0 || y <= 0) { //Implement error handling. The error callback pattern here is common in Node applications. Check if the arguments are invalid, if they are, handle the Error inside the "if" block.
+        callback(new Error(`Rectangle dimensions must be greater than zero. Received: ${x}, ${y}`)); //"callback" function will be written in app.js. Follows the Node error callback pattern: it expects an error object as its first argument. "Error" object is a global object in JS that contains information about an error. "new" keyword followed by "Error" creates a new error object with the message. As soon as the error condition is detected, the callback function will run.
+    } else { //If arguments are valid, the "callback" function will not run right away, "setTimeout" will simulate a situation where an asynchronous operation is happening (database is being read).
+        setTimeout(() => //As first argument for the "setTimeout" function, will pass an arrow function that contains the callback. 
+            callback(null, { //Because an error object is expected for first argument (as is the convention) will pass "null" since there is no error if program has made it this far.
+                //"callback" function expects a second argument, which is passed as an object in this case that contains two methods
+                perimeter: () => 2 * (x + y), //Don't need "x" and "y" passed in as arguments because of "closures" concept. The outer function variables are available to these inner functions already.
+                area: () => x * y
+            }),
+            2000
+        );
+    }
+};
